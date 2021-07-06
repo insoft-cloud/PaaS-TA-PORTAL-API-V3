a1f0f6df56bcc39143b8af09ff59c8d7a5841f0c
parkcheolhan <pch3770@gmail.com>
2021-07-06 15:45:36 +0900

    ServiceRouteBinding 항목 추가

---
+++ serviceroutebinding/controller.go
@@ -0,0 +1,134 @@
+package serviceroutebinding
+
+import (
+	"PAAS-TA-PORTAL-V3/config"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+var uris = "service_route_bindings"
+
+func ServiceRouteBindingHandleRequests(myRouter *mux.Router) {
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}", getServiceRouteBinding).Methods("GET")
+	myRouter.HandleFunc("/v3/"+uris, getServiceRouteBindings).Methods("GET")
+	myRouter.HandleFunc("/v3/"+uris, createServiceRouteBinding).Methods("POST")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}", updateServiceRouteBinding).Methods("PATCH")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}", deleteServiceRouteBinding).Methods("DELETE")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}/parameters", getServiceRouteBindingParameter).Methods("GET")
+}
+
+//Permitted roles 'Admin, Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+func getServiceRouteBinding(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	query, _ := url.QueryUnescape(r.URL.Query().Encode())
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"?"+query, nil, "GET", w, r)
+	if rBodyResult {
+		var final ServiceRouteBinding
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin, Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+func getServiceRouteBindings(w http.ResponseWriter, r *http.Request) {
+	query, _ := url.QueryUnescape(r.URL.Query().Encode())
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"?"+query, nil, "GET", w, r)
+	if rBodyResult {
+		var final ServiceRouteBindingList
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin, SpaceDeveloper'
+func createServiceRouteBinding(w http.ResponseWriter, r *http.Request) {
+	var pBody CreateServiceRouteBinding
+	vResultI, vResultB := config.Validation(r, &pBody)
+	if !vResultB {
+		json.NewEncoder(w).Encode(vResultI)
+		return
+	}
+	//호출
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, pBody)
+	reqBody, _ = json.Marshal(pBody)
+	rBody, rBodyResult := config.Curl("/v3/"+uris, reqBody, "POST", w, r)
+	if rBodyResult {
+		var final ServiceRouteBinding
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin Space Developer'
+func updateServiceRouteBinding(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	var pBody UpdateServiceRouteBinding
+	vResultI, vResultB := config.Validation(r, &pBody)
+	if !vResultB {
+		json.NewEncoder(w).Encode(vResultI)
+		return
+	}
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, pBody)
+	reqBody, _ = json.Marshal(pBody)
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid, reqBody, "PATCH", w, r)
+	if rBodyResult {
+		var final ServiceRouteBinding
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin Space Developer'
+func deleteServiceRouteBinding(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid, nil, "DELETE", w, r)
+	if rBodyResult {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin, Admin Read-Only Space Developer'
+func getServiceRouteBindingParameter(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"/parameters", nil, "GET", w, r)
+	if rBodyResult {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
