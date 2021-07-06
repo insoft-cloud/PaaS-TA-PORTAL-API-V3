57f1ae305e222aa38102008a07487cd2d7b74a6e
parkcheolhan <pch3770@gmail.com>
2021-07-06 11:17:06 +0900

    ServiceInstance 항목 추가

---
+++ serviceinstances/controller.go
@@ -0,0 +1,258 @@
+package serviceinstances
+
+import (
+	"PAAS-TA-PORTAL-V3/config"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+var uris = "service_instances"
+
+func ServiceInstanceHandleRequests(myRouter *mux.Router) {
+	myRouter.HandleFunc("/v3/"+uris, createServiceInstance).Methods("POST")
+	myRouter.HandleFunc("/v3/"+uris+"/user-provide", createServiceInstanceUserProvide).Methods("POST")
+	myRouter.HandleFunc("/v3/"+uris, getServiceInstances).Methods("GET")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}", getServiceInstance).Methods("GET")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}/credentials", getCredential).Methods("GET")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}/parameters", getParameter).Methods("GET")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}", updateServiceInstance).Methods("PATCH")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}", deleteServiceInstance).Methods("DELETE")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}/relationships/shared_spaces", getShareSpacesRelationship).Methods("GET")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}/relationships/shared_spaces", shareServiceInstance).Methods("POST")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}/relationships/shared_spaces/{space_guid}", unShareServiceInstance).Methods("DELETE")
+	myRouter.HandleFunc("/v3/"+uris+"/{guid}/relationships/shared_spaces/usage_summary", getUsageSummary).Methods("GET")
+}
+
+//Permitted roles 'Admin, SpaceDeveloper'
+func createServiceInstance(w http.ResponseWriter, r *http.Request) {
+	var pBody CreateServiceInstance
+	vResultI, vResultB := config.Validation(r, &pBody)
+	if !vResultB {
+		json.NewEncoder(w).Encode(vResultI)
+		return
+	}
+
+	//호출
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, pBody)
+	reqBody, _ = json.Marshal(pBody)
+	rBody, rBodyResult := config.Curl("/v3/"+uris, reqBody, "POST", w, r)
+	if rBodyResult {
+		var final ServiceInstance
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+func createServiceInstanceUserProvide(w http.ResponseWriter, r *http.Request) {
+	var pBody CreateServiceInstanceProvide
+	vResultI, vResultB := config.Validation(r, &pBody)
+	if !vResultB {
+		json.NewEncoder(w).Encode(vResultI)
+		return
+	}
+
+	//호출
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, pBody)
+	reqBody, _ = json.Marshal(pBody)
+	rBody, rBodyResult := config.Curl("/v3/"+uris, reqBody, "POST", w, r)
+	if rBodyResult {
+		var final ServiceInstance
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted All Roles
+func getServiceInstances(w http.ResponseWriter, r *http.Request) {
+	query, _ := url.QueryUnescape(r.URL.Query().Encode())
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"?"+query, nil, "GET", w, r)
+	if rBodyResult {
+		var final ServiceInstanceList
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin, Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+func getServiceInstance(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	query, _ := url.QueryUnescape(r.URL.Query().Encode())
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"?"+query, nil, "GET", w, r)
+	if rBodyResult {
+		var final ServiceInstance
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin, Admin Read-Only Space Developer Space Manager'
+func getCredential(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"/credentials", nil, "GET", w, r)
+	if rBodyResult {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin, Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+func getParameter(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"/parameters", nil, "GET", w, r)
+	if rBodyResult {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin Space Developer'
+func updateServiceInstance(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	var pBody UpdateServiceInstance
+	vResultI, vResultB := config.Validation(r, &pBody)
+	if !vResultB {
+		json.NewEncoder(w).Encode(vResultI)
+		return
+	}
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, pBody)
+	reqBody, _ = json.Marshal(pBody)
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid, reqBody, "PATCH", w, r)
+	if rBodyResult {
+		var final ServiceInstance
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin Space Developer'
+func deleteServiceInstance(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	query, _ := url.QueryUnescape(r.URL.Query().Encode())
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"?"+query, nil, "DELETE", w, r)
+	if rBodyResult {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+func getShareSpacesRelationship(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	query, _ := url.QueryUnescape(r.URL.Query().Encode())
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"/relationships/shared_spaces?"+query, nil, "GET", w, r)
+	if rBodyResult {
+		var final SharedSpacesRelationship
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin Space Developer'
+func shareServiceInstance(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	var pBody SharedServiceInstanceToOtherSpaces
+	vResultI, vResultB := config.Validation(r, &pBody)
+	if !vResultB {
+		json.NewEncoder(w).Encode(vResultI)
+		return
+	}
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, pBody)
+	reqBody, _ = json.Marshal(pBody)
+
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"/relationships/shared_spaces", reqBody, "POST", w, r)
+	if rBodyResult {
+		var final SharedSpacesRelationship
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin Space Developer'
+func unShareServiceInstance(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	spaceGuid := vars["space_guid"]
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"/relationships/shared_spaces/"+spaceGuid, nil, "DELETE", w, r)
+	if rBodyResult {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
+
+//Permitted roles 'Admin Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+func getUsageSummary(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	guid := vars["guid"]
+	rBody, rBodyResult := config.Curl("/v3/"+uris+"/"+guid+"/relationships/shared_spaces/usage_summary", nil, "GET", w, r)
+	if rBodyResult {
+		var final GetUsageSummaryInSharedSpace
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	} else {
+		var final interface{}
+		json.Unmarshal(rBody.([]byte), &final)
+		json.NewEncoder(w).Encode(final)
+	}
+}
