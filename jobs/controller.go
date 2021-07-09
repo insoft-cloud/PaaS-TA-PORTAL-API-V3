72982456b4133dbbc61717c1c78b4326dce63b34
yaoh125 <yaoh125>
2021-07-09 13:45:31 +0900

    jobs swagger 추가

--- jobs/controller.go
+++ jobs/controller.go
@@ -13,7 +13,18 @@ func JobsHandleRequests(myRouter *mux.Router) {
 	myRouter.HandleFunc("/v3/"+uris+"/{guid}", getJob).Methods("GET")
 }
 
-//Permitted Roles Admin Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manage
+//Permitted Roles Admin Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager
+// @Summary Get a job
+// @Description
+// @Tags Jobs
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param guid path string true "Job Guid"
+// @Success 200 {object} Jobs
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /jobs/{guid} [GET]
 func getJob(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	guid := vars["guid"]
