016806558637a0d676a201da81ddb3aee561df1a
parkcheolhan <pch3770@gmail.com>
2021-07-16 17:43:34 +0900

    swagger 적용

--- space_features/controller.go
+++ space_features/controller.go
@@ -17,6 +17,18 @@ func SpaceFeatureHandleRequests(myRouter *mux.Router) {
 }
 
 // @Description Permitted Roles 'Admin, Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+// @Summary Get a space feature
+// @Description
+// @Tags Space Features
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param guid path string true "Space Guid"
+// @Param name path string true "Feature Name"
+// @Success 200 {object} SpaceFeature
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces/{guid}/features/{name} [GET]
 func getSpaceFeature(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	guid := vars["guid"]
@@ -27,13 +39,21 @@ func getSpaceFeature(w http.ResponseWriter, r *http.Request) {
 		json.Unmarshal(rBody.([]byte), &final)
 		json.NewEncoder(w).Encode(final)
 	} else {
-		var final interface{}
-		json.Unmarshal(rBody.([]byte), &final)
-		json.NewEncoder(w).Encode(final)
+		json.NewEncoder(w).Encode(rBody)
 	}
 }
 
 // @Description Permitted Roles 'Admin, Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+// @Summary List space features
+// @Description This endpoint retrieves the list of features for the specified space. Currently, the only feature on spaces is the SSH feature.
+// @Tags Space Features
+// @Produce  json
+// @Security ApiKeyAuth
+// @Success 200 {object} SpaceFeatureList
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces/{guid}/features [GET]
 func getSpaceFeatures(w http.ResponseWriter, r *http.Request) {
 	rBody, rBodyResult := config.Curl("/v3/"+uris+"/features", nil, "GET", w, r)
 	if rBodyResult {
@@ -41,18 +61,29 @@ func getSpaceFeatures(w http.ResponseWriter, r *http.Request) {
 		json.Unmarshal(rBody.([]byte), &final)
 		json.NewEncoder(w).Encode(final)
 	} else {
-		var final interface{}
-		json.Unmarshal(rBody.([]byte), &final)
-		json.NewEncoder(w).Encode(final)
+		json.NewEncoder(w).Encode(rBody)
 	}
 }
 
 // @Description Permitted Roles 'Admin Space Developer'
+// @Summary Update space features
+// @Description
+// @Tags Space Features
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param guid path string true "Space Guid"
+// @Param name path string true "Feature Name"
+// @Param SpaceFeature body SpaceFeature true "Update SpaceFeature"
+// @Success 200 {object} SpaceFeature
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces/{guid}/features/{name} [PATCH]
 func updateSpaceFeature(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	guid := vars["guid"]
 	name := vars["name"]
-	var pBody interface{}
+	var pBody SpaceFeature
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(reqBody, pBody)
 	reqBody, _ = json.Marshal(pBody)
@@ -62,8 +93,6 @@ func updateSpaceFeature(w http.ResponseWriter, r *http.Request) {
 		json.Unmarshal(rBody.([]byte), &final)
 		json.NewEncoder(w).Encode(final)
 	} else {
-		var final interface{}
-		json.Unmarshal(rBody.([]byte), &final)
-		json.NewEncoder(w).Encode(final)
+		json.NewEncoder(w).Encode(rBody)
 	}
 }
--- spaces/controller.go
+++ spaces/controller.go
@@ -22,6 +22,17 @@ func ServiceRouteBindingHandleRequests(myRouter *mux.Router) {
 }
 
 // @Description Permitted Roles 'Admin, Org Manager'
+// @Summary Create a space
+// @Description
+// @Tags Spaces
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param Space path Space true "Create Space"
+// @Success 201 {object} Space
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces [POST]
 func createSpace(w http.ResponseWriter, r *http.Request) {
 	var pBody CreateSpace
 	vResultI, vResultB := config.Validation(r, &pBody)
@@ -46,6 +57,18 @@ func createSpace(w http.ResponseWriter, r *http.Request) {
 }
 
 // @Description Permitted Roles 'Admin Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+// @Summary Get a space
+// @Description This endpoint retrieves the specified space object.
+// @Tags Spaces
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param guid path string true "Space Guid"
+// @Param include query []string false "Optionally include additional related resources in the response; valid value is organization" collectionFormat(multi)
+// @Success 200 {object} Space
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces/{guid} [GET]
 func getSpace(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	guid := vars["guid"]
@@ -63,6 +86,26 @@ func getSpace(w http.ResponseWriter, r *http.Request) {
 }
 
 // @Description Permitted Roles 'All'
+// @Summary List spaces
+// @Description Retrieve all spaces the user has access to.
+// @Tags Spaces
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param names query []string false "Comma-delimited list of space names to filter by" collectionFormat(csv)
+// @Param guids query []string false "Comma-delimited list of space guids to filter by" collectionFormat(csv)
+// @Param organization_guids query []string false "Comma-delimited list of organization guids to filter by" collectionFormat(csv)
+// @Param page query integer false "Page to display; valid values are integers >= 1"
+// @Param per_page query integer false "Number of results per page; valid values are 1 through 5000"
+// @Param order_by query string false "Value to sort by. Defaults to ascending; prepend with - to sort descending. Valid values are created_at, updated_at, name, state"
+// @Param label_selector query string false "A query string containing a list of label selector requirements"
+// @Param include query []string false "Optionally include a list of unique related resources in the response; valid values are space and spaceorganization" collectionFormat(multi)
+// @Param created_ats query string false "Timestamp to filter by. When filtering on equality, several comma-delimited timestamps may be passed. Also supports filtering with relational operators"
+// @Param updated_ats query string false "Timestamp to filter by. When filtering on equality, several comma-delimited timestamps may be passed. Also supports filtering with relational operators"
+// @Success 200 {object} SpaceList
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces [GET]
 func getSpaces(w http.ResponseWriter, r *http.Request) {
 	query, _ := url.QueryUnescape(r.URL.Query().Encode())
 	rBody, rBodyResult := config.Curl("/v3/"+uris+"?"+query, nil, "GET", w, r)
@@ -78,6 +121,18 @@ func getSpaces(w http.ResponseWriter, r *http.Request) {
 }
 
 // @Description Permitted Roles 'Admin Org Manager Space Manager'
+// @Summary Update a space
+// @Description
+// @Tags Spaces
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param guid path string true "Space Guid"
+// @Param UpdateSpace path UpdateSpace true "Update Space"
+// @Success 200 {object} Space
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces/{guid} [PATCH]
 func updateSpace(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	guid := vars["guid"]
@@ -103,6 +158,17 @@ func updateSpace(w http.ResponseWriter, r *http.Request) {
 }
 
 // @Description Permitted Roles 'Admin Org Manager'
+// @Summary Delete a space
+// @Description When a space is deleted, the user roles associated with the space will be deleted.
+// @Tags Spaces
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param guid path string true "Space Guid"
+// @Success 202 {object} String "ok"
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces/{guid} [DELETE]
 func deleteSpace(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	guid := vars["guid"]
@@ -119,6 +185,17 @@ func deleteSpace(w http.ResponseWriter, r *http.Request) {
 }
 
 // @Description Permitted Roles 'Admin Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+// @Summary Get assigned isolation segment
+// @Description
+// @Tags Spaces
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param guid path string true "Space Guid"
+// @Success 200 {object} object
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces/{guid}/relationships/isolation_segment [GET]
 func assignedIsolationSegment(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	guid := vars["guid"]
@@ -135,6 +212,19 @@ func assignedIsolationSegment(w http.ResponseWriter, r *http.Request) {
 }
 
 // @Description Permitted Roles 'Admin Admin Read-Only Global Auditor Org Manager Space Auditor Space Developer Space Manager'
+// @Summary Manage isolation segment
+// @Description This endpoint assigns an isolation segment to the space. The isolation segment must be entitled to the space’s parent organization.
+// @Description Apps will not run in the newly assigned isolation segment until they are restarted.
+// @Tags Spaces
+// @Produce  json
+// @Security ApiKeyAuth
+// @Param guid path string true "Space Guid"
+// @Param IsolationSegment body IsolationSegment true "IsolationSegment"
+// @Success 200 {object} object
+// @Failure 400,404 {object} config.Error
+// @Failure 500 {object} config.Error
+// @Failure default {object} config.Error
+// @Router /spaces/{guid}/relationships/isolation_segment [PATCH]
 func manageIsolationSegment(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	guid := vars["guid"]
