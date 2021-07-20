9f489458e029b89510ab1218309c58cf6f75884a
parkcheolhan <pch3770@gmail.com>
2021-07-20 15:45:15 +0900

    ip 주소 받기(수정)

--- swagger.go
+++ swagger.go
@@ -1,31 +1,18 @@
 package main
 
 import (
+	"PAAS-TA-PORTAL-V3/config"
 	"PAAS-TA-PORTAL-V3/docs"
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
-	"net"
 )
 
 func main() {
 	//// programmatically set swagger info
-	iFaces, _ := net.Interfaces()
 	// handle err
-	docs.SwaggerInfo.Host = "localhost:2222"
-	for _, i := range iFaces {
-		addrs, _ := i.Addrs()
-		for _, addr := range addrs {
-			switch v := addr.(type) {
-			case *net.IPNet:
-				var ip net.IP
-				if v.IP.String() != "127.0.0.1" {
-					ip = v.IP
-					docs.SwaggerInfo.Host = ip.String() + ":2222"
-				}
-			}
-		}
-	}
+	config.SetConfig()
+	docs.SwaggerInfo.Host = config.Config["host"] + ":2222"
 	docs.SwaggerInfo.Title = "PaaS-TA Portal CF V3 API"
 	docs.SwaggerInfo.Description = "This is a PaaS-TA Portal Server."
 	docs.SwaggerInfo.Version = "3.0"
